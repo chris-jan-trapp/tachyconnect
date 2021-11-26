835f5a884f30cd4953c32a3f43efefa62a59b169
Oliver Zapiec <oliver.zapiec@gbv.de>
2021-11-26 12:29:00 +0100

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -1,3 +1,28 @@
-install_requires = [
-    "pyqt5",
-]
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tachyconnect-pkg-gbv",
+    version="0.0.2",
+    author="Christian Trapp",
+    author_email="trapp@gbv.de",
+    description="Leica total stations and Qt via python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gbv/tachyconnect",
+    project_urls={
+        "Bug Tracker": "https://github.com/gbv/tachyconnect/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha"
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=["pyqt5"]
+)
