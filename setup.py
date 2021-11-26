2c72e94c837b20c7647a1503d7a430bcf5800578
Oliver Zapiec <oliver.zapiec@gbv.de>
2021-11-26 13:28:20 +0100

    Bump version: 0.0.5 → 0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="tachyconnect-pkg-gbv",
-    version="0.0.5",
+    version="0.0.6",
     author="Christian Trapp",
     author_email="trapp@gbv.de",
     description="Leica total stations and Qt via python",
