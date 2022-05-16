8009539e944946f86f49dcb0415ceb0f6cb2575e
chris-jan-trapp <christian.trapp@gbv.de>
2022-05-16 13:05:17 +0200

    Bump version: 0.3.2 → 0.3.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="tachyconnect",
-    version="0.3.2",
+    version="0.3.3",
     author="Christian Trapp",
     author_email="trapp@gbv.de",
     description="Leica total stations and Qt via python",
